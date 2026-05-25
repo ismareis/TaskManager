b0df99c23a31702537c0b3ba69e1830fdbc00103
Ismael Reis <ismareis06@gmail.com>
2026-05-25 19:42:19 -0300

    chore: initial project setup

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/ping", (req, res) => {
+    res.json({ message: "pong" });
+});
+
+module.exports = app;
