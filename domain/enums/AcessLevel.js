0f4967aee8573466ecb2e3bbd3b042a42b476192
Ismael Reis <ismareis06@gmail.com>
2026-06-04 15:26:10 -0300

    fix: acessLevel correction

--- domain/enums/AcessLevel.js
+++ domain/enums/AcessLevel.js
@@ -1,12 +1,10 @@
 class AccessLevel {
     static USER = 1;
-    static MANAGER = 2;
-    static ADMIN = 3;
+    static ADMIN = 2;
 
     static isValid(value) {
         return [
             AccessLevel.USER,
-            AccessLevel.MANAGER,
             AccessLevel.ADMIN
         ].includes(value);
     }
