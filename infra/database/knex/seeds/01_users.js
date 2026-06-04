d4d49b064c999d4128eaebe0abce375447d0f1b1
Ismael Reis <ismareis06@gmail.com>
2026-06-04 16:23:42 -0300

    add: user seeds

---
+++ infra/database/knex/seeds/01_users.js
@@ -0,0 +1,25 @@
+const PasswordHasher = require('../../../services/PasswordHasher');
+const AccessLevel = require('../../../../domain/enums/AcessLevel');
+
+exports.seed = async function(knex) {
+    await knex('users').del();
+
+    await knex('users').insert([
+        {
+            name: 'System Administrator',
+            username: 'admin',
+            password: PasswordHasher.hash('admin123'),
+            role: 'Administrator',
+            access_level: AccessLevel.ADMIN,
+            disabled: false
+        },
+        {
+            name: 'Default User',
+            username: 'user',
+            password: PasswordHasher.hash('user123'),
+            role: 'User',
+            access_level: AccessLevel.USER,
+            disabled: false
+        }
+    ]);
+};
