694b2cfc7afb8dd2882c8d6a1a4e127e034718d0
Ismael Reis <ismareis06@gmail.com>
2026-06-05 23:03:39 -0300

    add: migration

---
+++ infra/database/knex/migrations/20240605183000_create_tasks.js
@@ -0,0 +1,29 @@
+const knex = require('knex');
+const TaskStatus = require('../../../../domain/enums/TaskStatus');
+const TaskPriority = require('../../../../domain/enums/TaskPriority');
+
+module.exports = {
+  up: async (knex) => {
+    await knex.schema.createTable('tasks', (table) => {
+      table.increments('id').primary();
+
+      table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE');
+      table.index('user_id');
+
+      table.string('title', 100).notNullable();
+      table.string('description', 1000).nullable();
+
+      table.integer('status').notNullable().checkIn(TaskStatus.All);
+      table.integer('priority').notNullable().checkIn(TaskPriority.All);
+
+      table.timestamp('due_date').notNullable();
+      table.timestamp('completion_date').nullable();
+
+      table.boolean('disabled').notNullable().defaultTo(false);
+    });
+  },
+
+  down: async (knex) => {
+    await knex.schema.dropTable('tasks');
+  },
+};
