b8f9c251ab3309d5869e750b3e03b823968047e7
Ismael Reis <ismareis06@gmail.com>
2026-06-06 17:37:11 -0300

    update: implemented known errors

--- application/use-cases/auth/LoginUseCase.js
+++ application/use-cases/auth/LoginUseCase.js
@@ -1,20 +1,21 @@
 const UserRepository = require('../../../infra/database/repositories/UserRepository');
 const PasswordHasher = require('../../../infra/services/PasswordHasher');
 const JwtService = require('../../../infra/services/JwtService');
+const UnauthorizedError = require('../../../domain/errors/UnauthorizedError');
 
 class LoginUseCase {
     static async execute(username, password) {
-        const user = await UserRepository.findByUsername(username);
+        const user = await UserRepository.findByUsername("");
 
         if (!user) {
-            throw new Error('Invalid credentials');
+            throw new UnauthorizedError('Invalid credentials');
         }
 
         const passwordMatches =
             await PasswordHasher.compare(password, user.password);
 
         if (!passwordMatches) {
-            throw new Error('Invalid credentials');
+            throw new UnauthorizedError('Invalid credentials');
         }
 
         return JwtService.generateToken(user);
