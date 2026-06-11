620f36ddb8c8c6fd8025ff388924838e96f84869
Ismael Reis <ismareis06@gmail.com>
2026-06-11 18:52:43 -0300

    /src directory created

--- src/infra/database/knex/knexFile.js
+++ src/infra/database/knex/knexFile.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 require('dotenv').config({
-  path: path.resolve(__dirname, '../../../.env')
+  path: path.resolve(__dirname, '../../../../.env')
 });
 
 module.exports = {
