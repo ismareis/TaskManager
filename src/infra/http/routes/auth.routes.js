4fc09ae4d2c00d9b40c13f3ae3eb65f95e5f2ab0
Ismael Reis <ismareis06@gmail.com>
2026-06-14 16:08:50 -0300

    add: google auth routes

--- src/infra/http/routes/auth.routes.js
+++ src/infra/http/routes/auth.routes.js
@@ -2,11 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const AuthController = require('../controllers/AuthController');
+const GoogleAuthController = require('../controllers/GoogleAuthController');
 const authMiddleware = require('../middleware/authMiddleware');
 const asyncHandler = require('../middleware/asyncHandler');
 
 router.post('/login', asyncHandler(AuthController.login));
 
 router.post('/logout', authMiddleware, asyncHandler(AuthController.logout));
+
+router.post('/google/login', authMiddleware, asyncHandler(GoogleAuthController.redirect));
+
+router.post('/google/logout', authMiddleware, asyncHandler(GoogleAuthController.logout));
+
+router.get('/google/callback', asyncHandler(GoogleAuthController.callback));
 
 module.exports = router;
