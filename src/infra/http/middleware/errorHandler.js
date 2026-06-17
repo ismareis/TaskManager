5691a3e55b4cffd201e9dd2bd7983d7efc859b31
Ismael Reis <ismareis06@gmail.com>
2026-06-17 09:34:57 -0300

    update: logger update

--- src/infra/http/middleware/errorHandler.js
+++ src/infra/http/middleware/errorHandler.js
@@ -1,17 +1,19 @@
 const DomainError = require('../../../domain/errors/DomainError');
+const LoggerService = require('../../services/LoggerService');
 
 function errorHandler(error, req, res, next) {
     if (error instanceof DomainError) {
-        console.error("Status: " + error.statusCode);
-        return res.status(error.statusCode).json({
+        res.status(error.statusCode);
+        LoggerService.error(req, res, error);
+        return res.json({
             message: error.message,
             errors: error.errors
         });
     }
 
-    console.error(error);
-
-    return res.status(500).json({
+    res.status(500);
+    LoggerService.error(req, res, error);
+    return res.json({
         message: 'Internal server error'
     });
 }
